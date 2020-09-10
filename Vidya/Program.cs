656d5f6000e54de1faa9e3923eb3a921b31e8fd4
Eskilll <eskil.nordlander@elev.ga.ntig.se>
2020-09-10 15:48:41 +0200

    initialCommit

---
+++ Vidya/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vidya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
